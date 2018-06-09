cc7f38bea02329949e674bba7fee21acd9ca91c5
yeyangyang_private <582871072@qq.com>
2018-06-09 13:34:16 +0800

    修改首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -14,6 +14,12 @@ Page({
     interval: 5000,
     duration: 1000
   },
+  /**
+   * 表单提交--搜索
+   */
+  formSubmit: function (e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+  },
 
   /**
    * 生命周期函数--监听页面加载
